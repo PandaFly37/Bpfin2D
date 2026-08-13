49045a5980d39d6cc2ddf9e2ac216dfb229aedc0
pandafly <dzh0312@outlook.com>
2026-08-13 15:53:05 +0800

    使用列表解析式优化读取后的处理

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 def read_code_file(target_file):
     with open(target_file, 'r', encoding='utf-8') as f:
         text = f.read()
-    text2 = text.split('\n')
-    ret = []
-    for i in text2:
-        if i == '':
-            continue
-        temp = []
-        for j in i:
-            temp.append(j)
-        ret.append(temp)
-    return ret
+    return [list(i) for i in text.split('\n') if i]
 
 # test
 if __name__ == '__main__':
